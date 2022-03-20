8bf81f85c8d1bee6c62a6c2c0b7816ebcc5a3e21
Karen Turner <krt@karenrenee.com>
2022-03-20 16:24:17 -0600

    Adding high C

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let note = (sender.currentTitle ?? "C") + "\(sender.tag)" 
+        animateButton(button: sender)
+        playSound(sound: note, type: "wav")
+    }
+    
+    func animateButton(button: UIButton) {
+        setButtonAlpha(object: button, alphaValue: 0.5)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {self.setButtonAlpha(object: button, alphaValue: 1.0)})
+    }
+    
+    func setButtonAlpha(object: UIButton, alphaValue: CGFloat) {
+        object.alpha = alphaValue
+    }
+    
+    func playSound(sound: String, type: String) {
+        if let path = Bundle.main.path(forResource: sound, ofType: type) {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                audioPlayer?.play()
+            }
+            catch {
+                print("Error Playing Sound")
+            }
+        }
+    }
     
 
 }
